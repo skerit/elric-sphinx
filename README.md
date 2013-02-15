2994bfffe360c10930402b816ff9492be0c14329
Jelle De Loecker <jelle@kipdola.be>
2013-02-15 23:08:29 +0200

    Initial plugin setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Elric Sphinx: Voice recognition plugin
+This is the voice recognition plugin for Elric
---
+++ sphinxPlugin.js
@@ -0,0 +1,11 @@
+var elric = {};
+
+var Sphinx = function Sphinx (elriclink) {
+
+	elric = elriclink;
+
+	var thisSphinx = this;
+
+}
+
+module.exports = Sphinx;
